b0e0584d501ab1e126949c0008fb508e4b8533e0
suisho <suisho000000@gmail.com>
2014-03-16 04:26:57 +0900

    v2

--- index.js
+++ index.js
@@ -3,54 +3,37 @@ var gutil = require('gulp-util')
 var unzip = require('unzip')
 var fs = require('fs')
 module.exports = function(){
-
-
   function transform(file, enc, callback){
-    var self = this
-    
-    // inner
-    function _unzip(stream, cb){
-      stream.pipe(unzip.Parse())
-            .on('entry', function(entry){
-              if(entry.type == "Directory"){
-                return
-              }
-
-              entry.pipe(through.obj(function(chunk, enc, cb){
-                //gutil.log("Find file: "+ entry.path)
-                self.push(new gutil.File({
-                  cwd : "./",
-                  path : entry.path,
-                  contents: chunk
-                }))
-              }))
-            }).on('close', function(){
-              cb();
-            })
-    }
-
-    //
-    // gulp
-    //
     if (file.isNull()) {
       this.push(file);
       return callback();
     }
-    console.log(file.pipe)
-    console.log(file.isBuffer())
-    console.log(file.path)
-
-    if (file.isBuffer()) {
-
-      var stream = fs.createReadStream(file.path)
-
-      return _unzip(stream, callback)
-    }
-
-    if (file.isStream()) {
-      return _unzip(file, callback)
-    }
-
+    
+    // unzip file
+    var self = this
+    file.pipe(unzip.Parse())
+      .on('entry', function(entry){
+        entry.pipe(through(function(chunk, enc, cb){
+          console.log("=============")
+          console.log(entry.path)
+          console.log(chunk.length)
+          gutil.log("Find file: "+ entry.path)
+          self.push(new gutil.File({
+            cwd : "./",
+            path : entry.path,
+            contents: chunk
+          }))
+        }))
+      })
+        /*.on('data', function(data){
+          console.log(data)
+        })
+        .on('end', function(){
+          console.log(entry.path)
+        })*/
+      .on('close', function(){
+        callback();
+      })
   }
   return through.obj(transform);
 }
