3b094cc3b14d7886b4be568acb90668edacb088d
ulises Gascon <ulisesgascongonzalez@gmail.com>
2019-05-30 13:22:05 +0200

    added linter

--- index.js
+++ index.js
@@ -1,51 +1,52 @@
-const Stream = require('stream'),
-  through = require('through2').obj,
-  unzip = require('unzipper'),
-  Vinyl = require('vinyl');
+const Stream = require('stream')
+
+const through = require('through2').obj
+
+const unzip = require('unzipper')
+
+const Vinyl = require('vinyl')
 
 module.exports = (options = {}) => {
-  
-
-  function transform(file, enc, callback){
+  function transform (file, enc, callback) {
     if (file.isNull()) {
-      this.push(file);
-      return callback();
+      this.push(file)
+      return callback()
     }
 
-    let stream = new Stream.PassThrough();
+    let stream = new Stream.PassThrough()
 
-		if ( file.isBuffer() && !file.pipe ) {
-			stream.end( file.contents );
-		} else {
-      stream = file;
+    if (file.isBuffer() && !file.pipe) {
+      stream.end(file.contents)
+    } else {
+      stream = file
     }
 
-    const opts = {};
-    opts.filter = options.filter || (() => true);
-    opts.keepEmpty = options.keepEmpty || false;
+    const opts = {}
+    opts.filter = options.filter || (() => true)
+    opts.keepEmpty = options.keepEmpty || false
 
     stream.pipe(unzip.Parse())
       .on('entry', entry => {
-        const chunks = [];
-        if(!opts.filter(entry)){
-          entry.autodrain();
-          return;
+        const chunks = []
+        if (!opts.filter(entry)) {
+          entry.autodrain()
+          return
         }
-        
+
         entry.pipe(through((chunk, enc, cb) => {
-          chunks.push(chunk);
-          cb();
+          chunks.push(chunk)
+          cb()
         }, cb => {
-          if(entry.type === 'File' && (chunks.length > 0 || opts.keepEmpty)){
+          if (entry.type === 'File' && (chunks.length > 0 || opts.keepEmpty)) {
             this.push(new Vinyl({
-              cwd : "./",
-              path : entry.path,
+              cwd: './',
+              path: entry.path,
               contents: Buffer.concat(chunks)
-            }));
+            }))
           }
-          cb();
-        }));
-      }).on('close', callback);
+          cb()
+        }))
+      }).on('close', callback)
   }
-  return through(transform);
-};
+  return through(transform)
+}
--- test.js
+++ test.js
@@ -1,130 +1,133 @@
-const unzip = require('./index.js'),
-  fs = require('fs'),
-  assert = require('assert-plus'),
-  pj = require('path').join,
-  minimatch = require('minimatch'),
-  Vinyl = require('vinyl');
+/* eslint-env mocha */
 
-function createVinyl(filename, contents) {
-  const base = pj(__dirname, 'fixture');
-  const filePath = pj(base, filename);
+const unzip = require('./index.js')
+
+const fs = require('fs')
+
+const assert = require('assert-plus')
+
+const pj = require('path').join
+
+const minimatch = require('minimatch')
+
+const Vinyl = require('vinyl')
+
+function createVinyl (filename, contents) {
+  const base = pj(__dirname, 'fixture')
+  const filePath = pj(base, filename)
 
   return new Vinyl({
     cwd: __dirname,
     base: base,
     path: filePath,
     contents: contents || fs.readFileSync(filePath)
-  });
+  })
 }
 
 describe('gulp-unzip', () => {
-  
   describe('basic behaviour', () => {
-    
-    it("null file", done => {
-      const stream = unzip();
-      const mock = new Vinyl();
-      
-      stream.on('data', file => {
-        assert.deepEqual(file, mock);
-        done();
-      });
-      stream.write(mock);
-      stream.end();
-    });
-    
-    it("basic", done => {
-      const stream = unzip();
-      const mock = createVinyl('basic/zipped.zip');
-      
-      stream.on('data', file => {
-        const expect = fs.readFileSync('fixture/basic/' + file.path);
-        assert.deepEqual(expect, file.contents);
-      }).on('end', done);
-      stream.write(mock);
-      stream.end();
-    });
-    
-    it("large_file", done => {
-      const stream = unzip();
-      const mock = createVinyl('largefile/zipped.zip');
-      
-      stream.on('data', file => {
-        const expect = fs.readFileSync('fixture/largefile/' + file.path);
-        assert.deepEqual(expect, file.contents);
-      }).on('end', done);
-      stream.write(mock);
-      stream.end();
-    });
-  });
-  
-  describe("filter option", () => {
-    
-    it("false filter", done => {
-      const stream = unzip({
-        filter : () => false
-      });
-      const mock = createVinyl('basic/zipped.zip');
-      
-      stream.on('data', file => {
-        throw "entried";
-      }).on('end', done);
-      stream.write(mock);
-      stream.end();
-    });
+    it('null file', done => {
+      const stream = unzip()
+      const mock = new Vinyl()
 
-    it("only extract css with filter", done => {
-      const stream = unzip({
-        filter: entry => minimatch(entry.path, "**/*.min.css")
-      });
-      const mock = createVinyl('sometypes/bootstrap-3.1.1-dist.zip');
-      
       stream.on('data', file => {
-        assert.ok(/.*\.min\.css/.test(file.path));
-      }).on('end', done);
-      stream.write(mock);
-      stream.end();
-    });
-  });
+        assert.deepEqual(file, mock)
+        done()
+      })
+      stream.write(mock)
+      stream.end()
+    })
 
-  describe("keepEmpty option", () => {
-    
-    it("true", done => {
-      const stream = unzip({ keepEmpty: false });
-      const mock = createVinyl('basic/zipped.zip');
-      let didSeeEmpty = false;
+    it('basic', done => {
+      const stream = unzip()
+      const mock = createVinyl('basic/zipped.zip')
+
+      stream.on('data', file => {
+        const expect = fs.readFileSync('fixture/basic/' + file.path)
+        assert.deepEqual(expect, file.contents)
+      }).on('end', done)
+      stream.write(mock)
+      stream.end()
+    })
+
+    it('large_file', done => {
+      const stream = unzip()
+      const mock = createVinyl('largefile/zipped.zip')
+
+      stream.on('data', file => {
+        const expect = fs.readFileSync('fixture/largefile/' + file.path)
+        assert.deepEqual(expect, file.contents)
+      }).on('end', done)
+      stream.write(mock)
+      stream.end()
+    })
+  })
+
+  describe('filter option', () => {
+    it('false filter', done => {
+      const stream = unzip({
+        filter: () => false
+      })
+      const mock = createVinyl('basic/zipped.zip')
+
+      stream.on('data', file => {
+        throw Error('entried')
+      }).on('end', done)
+      stream.write(mock)
+      stream.end()
+    })
+
+    it('only extract css with filter', done => {
+      const stream = unzip({
+        filter: entry => minimatch(entry.path, '**/*.min.css')
+      })
+      const mock = createVinyl('sometypes/bootstrap-3.1.1-dist.zip')
+
+      stream.on('data', file => {
+        assert.ok(/.*\.min\.css/.test(file.path))
+      }).on('end', done)
+      stream.write(mock)
+      stream.end()
+    })
+  })
+
+  describe('keepEmpty option', () => {
+    it('true', done => {
+      const stream = unzip({ keepEmpty: false })
+      const mock = createVinyl('basic/zipped.zip')
+      let didSeeEmpty = false
 
       stream.on('data', file => {
         if (file.contents.length === 0) {
-          didSeeEmpty = true;
+          didSeeEmpty = true
         }
       }).on('end', () => {
         if (didSeeEmpty) {
-          throw new Error('saw empty');
+          throw new Error('saw empty')
         }
-        done();
-      });
-      stream.write(mock);
-      stream.end();
-    });
+        done()
+      })
+      stream.write(mock)
+      stream.end()
+    })
 
-    it("false", done => {
-      const stream = unzip({ keepEmpty: true });
-      const mock = createVinyl('basic/zipped.zip');
-      let didSeeEmpty = false;
+    it('false', done => {
+      const stream = unzip({ keepEmpty: true })
+      const mock = createVinyl('basic/zipped.zip')
+      let didSeeEmpty = false
 
       stream.on('data', file => {
         if (file.contents.length === 0) {
-          didSeeEmpty = true;
+          didSeeEmpty = true
         }
       }).on('end', () => {
         if (!didSeeEmpty) {
-          throw new Error('did not see empty');
+          throw new Error('did not see empty')
         }
-        done();
-      });
-      stream.write(mock);
-      stream.end();
-    });
-  });
-});
+        done()
+      })
+      stream.write(mock)
+      stream.end()
+    })
+  })
+})
